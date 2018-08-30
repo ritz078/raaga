7e8f95e319bd99f62e1bbfdac065cc25d87582c5
ritz078 <rkritesh078@gmail.com>
2018-08-30 21:24:11 +0530

    feat: init project

---
+++ next.config.js
@@ -0,0 +1,9 @@
+const withTypescript = require('@zeit/next-typescript');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+module.exports = withTypescript({
+  webpack(config, options) {
+    if (options.isServer) config.plugins.push(new ForkTsCheckerWebpackPlugin())
+    return config
+  }
+});
