584c5e0ef9161714948acafa299772c949dbdde1
Ritesh Kumar <ritz078@users.noreply.github.com>
2018-11-04 20:49:29 +0530

    chore(fallback): create a fallback for offscreenCanvas (#8)

--- workers/canvas.worker.ts
+++ workers/canvas.worker.ts
@@ -1,55 +1,5 @@
-import {
-  VISUALIZER_MESSAGES,
-  VISUALIZER_MODE
-} from "@enums/visualizerMessages";
-import { Track } from "midiconvert";
-import { Visualizer } from "@utils/Visualizer";
-import { Dimensions, Range } from "@utils/typings/Visualizer";
-
-let visualizer, intervalId;
-
-interface Data {
-  canvas: {
-    getContext: (x: string) => CanvasRenderingContext2D;
-  };
-  track: Track;
-  message: VISUALIZER_MESSAGES;
-  range: Range;
-  dimensions: Dimensions;
-  midi: number;
-  mode: VISUALIZER_MODE;
-}
+import { controlVisualizer } from "@utils/visualizerControl";
 
 self.onmessage = e => {
-  const {
-    canvas,
-    track,
-    message,
-    range,
-    dimensions,
-    midi,
-    mode
-  }: Data = e.data;
-
-  if (message === VISUALIZER_MESSAGES.INIT) {
-    clearInterval(intervalId);
-    visualizer = new Visualizer(canvas, dimensions, range, mode);
-  } else if (message === VISUALIZER_MESSAGES.UPDATE_DIMENSIONS) {
-    visualizer.setDimensions(dimensions);
-  } else if (message === VISUALIZER_MESSAGES.UPDATE_RANGE) {
-    visualizer.setRange(range);
-  } else if (message === VISUALIZER_MESSAGES.PLAY_TRACK) {
-    visualizer.setRange(range);
-    visualizer.play(track);
-  } else if (message === VISUALIZER_MESSAGES.STOP_TRACK) {
-    visualizer.cleanup();
-  } else if (message === VISUALIZER_MESSAGES.PLAY_NOTE) {
-    visualizer.addNote(midi);
-  } else if (message === VISUALIZER_MESSAGES.STOP_NOTE) {
-    visualizer.endNote(midi);
-  } else if (message === VISUALIZER_MESSAGES.SET_MODE) {
-    visualizer.setMode(mode);
-  } else if (message === VISUALIZER_MESSAGES.TOGGLE) {
-    visualizer.toggle();
-  }
+  controlVisualizer(e.data);
 };
