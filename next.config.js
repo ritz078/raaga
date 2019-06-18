0b040deb4d9d480eb89137c9dc8919036b4de982
Ritesh Kumar <ritz078@users.noreply.github.com>
2019-06-19 00:49:11 +0530

    feat(sw): added service worker for offline caching (#41)
    * feat(sw): added service worker for offline caching
    
    closes #21
    
    * fixes
    
    * create sw in dev mode

--- next.config.js
+++ next.config.js
@@ -2,36 +2,56 @@ const withTypescript = require("@zeit/next-typescript");
 const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 const withCSS = require("@zeit/next-css");
 const webpack = require("webpack");
+const withOffline = require("next-offline");
 
-module.exports = withCSS(
-  withTypescript({
-    webpack(config, options) {
-      if (options.isServer && options.dev)
-        config.plugins.push(new ForkTsCheckerWebpackPlugin());
+const config = {
+  webpack(config, options) {
+    if (options.isServer && options.dev)
+      config.plugins.push(new ForkTsCheckerWebpackPlugin());
 
-      config.plugins.push(
-        new webpack.DefinePlugin({
-          "process.env.DEV": JSON.stringify(options.dev)
-        })
-      );
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        "process.env.DEV": JSON.stringify(options.dev)
+      })
+    );
 
-      config.module.rules.unshift({
-        test: /\.worker\.ts/,
-        use: {
-          loader: "worker-loader",
-          options: { fallback: true, inline: true }
+    config.module.rules.unshift({
+      test: /\.worker\.ts/,
+      use: {
+        loader: "worker-loader",
+        options: { fallback: true, inline: true }
+      }
+    });
+
+    config.output.globalObject = `this`;
+
+    return config;
+  },
+
+  target: "serverless",
+
+  generateInDevMode: true,
+
+  workboxOpts: {
+    swDest: "static/service-worker.js",
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: "NetworkFirst",
+        options: {
+          cacheName: "https-calls",
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
+          },
+          cacheableResponse: {
+            statuses: [0, 200]
+          }
         }
-      });
+      }
+    ]
+  }
+};
 
-      config.output.globalObject = `this`;
-
-      return config;
-    },
-
-    exportPathMap: async function() {
-      return {
-        "/": { page: "/" }
-      };
-    }
-  })
-);
+module.exports = withOffline(withCSS(withTypescript(config)));
