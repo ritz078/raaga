83c0841d5e8a39c7ad965921301030c28690c5d1
Ritesh Kumar <ritz078@users.noreply.github.com>
2019-06-18 19:22:14 +0530

    Header (#39)
    * feat(beta): added beta tag on header
    
    * refactor

--- config/piano.ts
+++ config/piano.ts
@@ -1,6 +1,6 @@
-export const TRACK_PLAYING_SPEED = 250;
+export const TRACK_PLAYING_SPEED = 100;
 export const HORIZONTAL_GAP_BETWEEN_NOTES = 2;
 export const MS_PER_SECOND = 10;
-export const PIANO_HEIGHT = 210;
+export const PIANO_HEIGHT = 250;
 export const ACCIDENTAL_KEY_COLOR = "#ffdc66";
 export const NATURAL_KEY_COLOR = "#42C9FF";
