8a06d88462a6454004454f723969485fe30bd08a
Ritesh Kumar <riteshkr.hello@gmail.com>
2019-09-14 15:57:27 +0530

    fix(nprogress): don't purge CSS in production

--- postcss.config.js
+++ postcss.config.js
@@ -4,7 +4,8 @@ const purgecss = require("@fullhuman/postcss-purgecss")({
   content: ["./pages/**/*.tsx", "./components/**/*.tsx"],
 
   // Include any special characters you're using in this regular expression
-  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+  whitelistPatternsChildren: [/nprogress$/]
 });
 
 const isProduction = process.env.NODE_ENV === "production";
