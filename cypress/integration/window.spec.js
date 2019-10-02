6bfe814cbee102ba4961bd0024ccfb39fadbc4a1
Majid Hajian <mhadaily@gmail.com>
2019-10-02 11:12:53 +0200

    setup cypress and add one integration test

---
+++ cypress/integration/window.spec.js
@@ -0,0 +1,21 @@
+/// <reference types="Cypress" />
+
+context("Window", () => {
+  beforeEach(() => {
+    cy.visit("https://raaga.io");
+  });
+
+  it("cy.window() - get the global window object", () => {
+    cy.window().should("have.property", "top");
+  });
+
+  it("cy.document() - get the document object", () => {
+    cy.document()
+      .should("have.property", "charset")
+      .and("eq", "UTF-8");
+  });
+
+  it("cy.title() - get the title", () => {
+    cy.title().should("include", "Raaga | See · Play · Learn");
+  });
+});
