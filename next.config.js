cfacad5b57e7e27980d931ceb6d43a533c639f34
Prateek Bhatnagar <prateek89born@gmail.com>
2020-08-10 09:57:58 -0700

    Run optimizeFonts only for production build (#110)
    * importing AudioContext, IntersceonObserver
    
    * adding polyfills in 1P
    
    * Update pages/_app.tsx
    
    * adding experimental fonts options
    
    * removing unwanted packages
    
    * updating next
    
    * removing web-vitals
    
    * optimizeFonts only during production build
    
    * fixing next.config.js
    
    * lint fix
    
    Co-authored-by: Ritesh Kumar <ritz078@users.noreply.github.com>

--- next.config.js
+++ next.config.js
@@ -1,60 +1,63 @@
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
 const withCSS = require("@zeit/next-css");
 const webpack = require("webpack");
 
-const config = {
-  experimental: {
-    optimizeFonts: true
-  },
-  webpack(config, options) {
-    config.plugins.push(
-      new webpack.DefinePlugin({
-        "process.env.DEV": JSON.stringify(options.dev),
-        IN_BROWSER: !options.isServer,
-        IS_DEV: options.dev
-      })
-    );
-
-    config.module.rules.unshift(
-      {
-        test: /\.worker\.ts/,
-        use: {
-          loader: "worker-loader",
-          options: { fallback: true, inline: true }
-        }
+module.exports = phase => {
+  return withCSS(
+    {
+      experimental: {
+        optimizeFonts: phase!==PHASE_DEVELOPMENT_SERVER
       },
-      {
-        test: /\.svg$/,
-        use: [
+      webpack(config, options) {
+        config.plugins.push(
+          new webpack.DefinePlugin({
+            "process.env.DEV": JSON.stringify(options.dev),
+            IN_BROWSER: !options.isServer,
+            IS_DEV: options.dev
+          })
+        );
+
+        config.module.rules.unshift(
           {
-            loader: "@svgr/webpack",
-            options: {
-              svgoConfig: {
-                plugins: {
-                  removeViewBox: false
-                }
-              }
+            test: /\.worker\.ts/,
+            use: {
+              loader: "worker-loader",
+              options: { fallback: true, inline: true }
             }
           },
           {
-            loader: "url-loader"
+            test: /\.svg$/,
+            use: [
+              {
+                loader: "@svgr/webpack",
+                options: {
+                  svgoConfig: {
+                    plugins: {
+                      removeViewBox: false
+                    }
+                  }
+                }
+              },
+              {
+                loader: "url-loader"
+              }
+            ]
           }
-        ]
-      }
-    );
+        );
 
-    config.output.globalObject = 'typeof self !== "object" ? self : this';
+        config.output.globalObject = 'typeof self !== "object" ? self : this';
 
-    // Temporary fix for https://github.com/zeit/next.js/issues/8071
-    config.plugins.forEach(plugin => {
-      if (plugin.definitions && plugin.definitions["typeof window"]) {
-        delete plugin.definitions["typeof window"];
-      }
-    });
+        // Temporary fix for https://github.com/zeit/next.js/issues/8071
+        config.plugins.forEach(plugin => {
+          if (plugin.definitions && plugin.definitions["typeof window"]) {
+            delete plugin.definitions["typeof window"];
+          }
+        });
 
-    return config;
-  },
+        return config;
+      },
 
-  target: "serverless"
+      target: "serverless"
+    }
+  )
 };
-
-module.exports = withCSS(config);
