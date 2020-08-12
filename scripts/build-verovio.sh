f3a8d152e955506c1db3de08b9230e49b9e95858
ritz078 <rkritesh078@gmail.com>
2020-08-13 03:38:29 +0530

    fixes

--- scripts/build-verovio.sh
+++ scripts/build-verovio.sh
@@ -4,10 +4,10 @@ set -x
 
 ROOT_DIR="$(cd "$(dirname "$1")" && pwd -P)/$(basename "$1")"
 cd "${ROOT_DIR}node_modules/verovio/tools"
-sudo apt-get install build-essential cmake
+apt-get install build-essential cmake
 cmake ../cmake
 make
-sudo make install
+make install
 echo
 rm -rf "${ROOT_DIR}public/bin"
 npx recursive-copy-cli ./verovio "${ROOT_DIR}public/bin/verovio/verovio" -w
