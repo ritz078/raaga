1846996c88c30f41bc721af405742050aa927d30
Sruthi <37189469+sruthiragupathy@users.noreply.github.com>
2021-02-22 00:19:14 +0530

    refactor-Move folders to src (#126)
    * refactor-Move folders to src
    
    * fix-fix the path in index.tsx
    
    * refactor - Restructure the project
    
    * Refactor-Made the suggested changes
    
    * Update src/pages/index.tsx
    
    Co-authored-by: Ritesh Kumar <ritz078@users.noreply.github.com>

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 // postcss.config.js
 const purgecss = require("@fullhuman/postcss-purgecss")({
   // Specify the paths to all of the template files in your project
-  content: ["./pages/**/*.tsx", "./components/**/*.tsx"],
+  content: ["./src/pages/**/*.tsx", "./src/components/**/*.tsx"],
 
   // Include any special characters you're using in this regular expression
   defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
