721fb0a812d7acfa9f16b945092e668c5dbc5466
Ritesh Kumar <ritz078@users.noreply.github.com>
2022-11-02 02:09:58 +0530

    code refactoring

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -2,7 +2,6 @@
 // This example commands.js shows you how to
 // create various custom commands and overwrite
 // existing commands.
-//
 // For more comprehensive examples of custom
 // commands please read more here:
 // https://on.cypress.io/custom-commands
